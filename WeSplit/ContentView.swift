c0b50338ff1d764330c0215b12fdbef7c46aeea3
Jeffrey Carpenter <spaz926@me.com>
2020-02-14 22:58:27 -0600

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Jeffrey Carpenter on 2/14/20.
+//  Copyright © 2020 Jeffrey Carpenter. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
